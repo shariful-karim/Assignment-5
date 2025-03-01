f374f9096210855d00178597202eb4c6fadbd151
sharif <sharifulkarim468@gmail.com>
2025-03-02 02:02:56 +0600

    js.project

--- problem.js
+++ problem.js
@@ -1,12 +1,19 @@
 const button = document.querySelectorAll('.btn');
-
+const task=document.getElementById('task')
+const checkbox=document.getElementById('checkbox')
 for(let i =0; i<button.length; i++){
 
     const allButton= button[i];
 
     allButton.addEventListener('click',function(event){
         
-    event.target.classList.add('bg-gray-200');
-
+    // event.target.classList.add('bg-gray-200');
+      alert("Bord updated Succesfully")
+      checkbox.innerText=parseInt(checkbox.innerText)+1;
+      task.innerText=parseInt(task.innerText)-1;
     })
+
+   
 }
+
+
