44019e6d3920788b5200bc31f11c006d4f2008a4
sharif <sharifulkarim468@gmail.com>
2025-03-02 09:30:27 +0600

    js.project

--- problem.js
+++ problem.js
@@ -15,15 +15,16 @@ for(let i =0; i<button.length; i++){
       event.target.classList.add('bg-gray-200');
      allButton.setAttribute("disabled",true);
       
-    })
-  allButton.addEventListener('click',function(event){
-    const history=document.getElementById('history');
-   const historye =document.createElement('p').classList('bg-red-300');
-   historye.innerText="hdsjahdghsg";
 
-   history.appendChild(historye)
-  })
+     const history=document.querySelector('.history');
    
+    const historye =document.createElement('p');
+    historye.classList.add('box');
+    historye.innerText="You have Complete The Task Add Dark Mode at 12:48:15 PM";
+ 
+    history.appendChild(historye);
+    })
+  
 }
 
 const newToday=document.querySelector('.newToday');
@@ -33,13 +34,13 @@ newToday.addEventListener('click',function(event){
   document.body.style.background=`#${hexVal}`
 })
 
-
-
-
 const today=document.querySelector('.today');
 today.addEventListener('click',function(event){
   window.location.href="./blogs.html"
 })
 
-
-
+const clear=document.querySelector('.clear');
+clear.addEventListener('click',function(event){
+ const clear= document.querySelector('.history').innerHTML="";
+ 
+})
